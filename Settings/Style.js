7d79a3729660ef41d7c9d254c4e74408a87daeb8
LoomisM4 <github@marcel-wettach.com>
2019-05-26 17:32:22 +0200

    Login Design verbessert

--- Settings/Style.js
+++ Settings/Style.js
@@ -3,5 +3,29 @@ import {StyleSheet} from "react-native";
 export const styles = StyleSheet.create({
     view: {
         flex: 1,
+    },
+
+    content: {
+        padding: 20,
+    },
+
+    textInput: {
+        width: '80%',
+        height: 40,
+        borderColor: 'grey',
+        borderWidth: 1,
+        marginBottom: 5,
+        paddingLeft: 5,
+        paddingRight: 5,
+        alignSelf: 'center'
+    },
+
+    listItem: {
+        fontSize: 20,
+        margin: 5
+    },
+
+    center: {
+        alignSelf: 'center'
     }
 });
--- Views/Login.js
+++ Views/Login.js
@@ -1,28 +1,34 @@
 import React, {Component} from 'react';
-import {Button, SafeAreaView, TextInput} from "react-native";
+import {Button, SafeAreaView, TextInput, Text, TouchableHighlight} from "react-native";
 import {styles} from "../Settings/Style";
 
 export class Login extends Component {
+    static navigationOptions = {
+        header: null
+    }
+
     render() {
         return (
-            <SafeAreaView style={styles.view}>
-                <TextInput
+            <SafeAreaView style={[styles.view, {marginTop: '70%'}]}>
+                <TextInput style={styles.textInput}
                     placeholder={"E-Mail"}
                     textContentType={"emailAddress"}
                 />
-                <TextInput
+                <TextInput style={styles.textInput}
                     placeholder={"Password"}
                     textContentType={"password"}
                     secureTextEntry={true}
                 />
-                <Button
-                    onPress={login}
+                <Button style={styles.content}
+                    onPress={_ => this.props.navigation.navigate("Overview")}
                     title={"Login"}
                 />
-                <Button
-                    title={"Registrieren"}
-                    onPress={_ => this.props.navigation.navigate("Register")}
-                />
+                <Text style={[styles.center, {marginTop: 5}]}>oder
+                    <Text
+                        style={{color: 'blue'}}
+                        onPress={_ => this.props.navigation.navigate("Register")}> Registrieren
+                    </Text>
+                </Text>
             </SafeAreaView>
         );
 
